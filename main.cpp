d33c7db9d21c842a3cd23fbdb8137ac2d669c137
kongdeying <kongdeying@it-sunao.com>
2024-06-06 11:41:37 +0800

    分支修改

--- main.cpp
+++ main.cpp
@@ -3,16 +3,5 @@
 
 int main()
 {
-	cout << "Hello World" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，Qt" << endl;
-	cout << "Hello World" << endl;
-
-
-	mainwindow.show();
+	cout << "fix-bug 分支修改" << endl;
 }
