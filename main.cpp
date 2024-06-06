31aaa6b98b218029e5f4471e79d259c9cb7d4d60
kongdeying <kongdeying@it-sunao.com>
2024-06-06 11:40:35 +0800

    主线修改

--- main.cpp
+++ main.cpp
@@ -3,16 +3,5 @@
 
 int main()
 {
-	cout << "Hello World" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，世界" << endl;
-	cout << "你好，Qt" << endl;
-	cout << "Hello World" << endl;
-
-
-	mainwindow.show();
+	cout << "主线修改" << endl;
 }
